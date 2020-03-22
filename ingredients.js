c4b3801d759e7c86f8e0f1f149462ce88d88e66c
Eduardo <Eduardo0321@gmail.com>
2020-03-22 03:29:17 +0000

    Banana bread loop challenge

--- ingredients.js
+++ ingredients.js
@@ -5,6 +5,11 @@ while(i < ingredients.length) {
   console.log(ingredients[i])
   i++;
 }
-// Write a for loop that prints out the contents of ingredients:
 
-// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i])
+}
+
+for (let i = 7; i >= 0  ; i--) {
+  console.log(ingredients[i])
+}
